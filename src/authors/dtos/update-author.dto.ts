67103c9165daac876289869455d30ab00d76cd5f
Dariusz Biernat <kaiju83@outlook.com>
2023-11-29 21:30:40 +0100

    Add modules author, book, create endpoints

---
+++ src/authors/dtos/update-author.dto.ts
@@ -0,0 +1,19 @@
+import { Transform } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export class UpdateAuthorDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 100)
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 3)
+  country: string;
+
+}
---
+++ src/books/dtos/create-book.dto.ts
@@ -0,0 +1,35 @@
+import { Transform } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsString,
+  Length,
+  IsInt,
+  Min,
+  Max,
+  IsUUID
+} from 'class-validator';
+
+export class CreateBookDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 100)
+  title: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
+  @Max(1000)
+  price: number;
+
+  @IsNotEmpty()
+  @IsUUID()
+  @IsString()
+  authorId: string;
+
+}
